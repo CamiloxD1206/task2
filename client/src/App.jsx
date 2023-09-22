17603c2a85f0985fa748c9263446470e1bda5317
camilo <cetacue@misena.edu.co>
2023-09-22 18:36:43 -0500

    2:20:11

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 function App () {
   return(
-    <div>App</div>
+   <BrowserRouter>
+   <Routes>
+   < Route path='/' element={<h1>Home page</h1>} />
+   < Route path='/login' element={<LoginPage />} />
+   < Route path='/register' element={<RegisterPage/>} />
+   < Route path='/tasks' element={<h1>tasks</h1>} />
+   < Route path='add-tasks' element={<h1>añadir tarea</h1>} />
+   < Route path='/tasks/:id' element={<h1>actualizar tarea</h1>} />
+   < Route path='/profile' element={<h1>perfil</h1>} />
+   </Routes>
+   </BrowserRouter>
   )
   
 }
